0a37db8c727db6d55abb09a2512a691cfa7d356c
thesave <thesave86@gmail.com>
2016-04-20 22:53:46 +0200

    added support for Linux build

---
+++ LaunchLinuxTerminal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# sourcing different "export" files
+for file in ~/.bashrc ~/.bash_profile ~/.bash_export
+do
+	if [[ -r $file ]]; then
+		cmd="$cmd source $file;"
+	fi
+done
+# adding cd to file working path
+cmd="$cmd cd $1;"
+
+# adding jolie launch command
+cmd="$cmd jolie $2;"
+
+# keeps the terminal open
+cmd="$cmd bash -i"
+
+x-terminal-emulator -e "bash -c '$cmd'"
